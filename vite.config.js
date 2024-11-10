23cf0069d4dd8bfa59f06d81b619f9476c88f30b
Andrey Golovachev (moonzaki) <dev.moonzaki@yandex.ru>
2024-11-10 23:38:29 +0300

    fix: change path to sprite; issues: https://github.com/vitejs/vite/issues/4454 ; add icon to  favicon;

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/your-repo-name/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/notes-app/' : '/',
   plugins: [react()]
 });
